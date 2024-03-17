be27d5b5e2cbd2758aa659ba72f47e8af8d4c9bc
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-03-17 17:13:59 -0300

    Aula 03 - Exercicios Casos Teste - Roman Numeral

---
+++ Aula03__Introducao_a_testes_unitarios/ExerciciosCasosTeste/TestCases/RomanNumeral.md
@@ -0,0 +1,7 @@
+| Input | Expected Output | Actual Output | Pass/Fail |
+| --- | --- | --- | --- |
+| I | 1 | 1 | Pass |
+| V | 5 | 5 | Pass |
+| IV | 4 | 4 | Pass |
+| IIII | Error | 4 | Fail |
+| IVVI | Error | 10 | Fail |
--- Aula03__Introducao_a_testes_unitarios/ExerciciosCasosTeste/src/App.java
+++ Aula03__Introducao_a_testes_unitarios/ExerciciosCasosTeste/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        testRomanNumerals();
     }
 
     private static void evaluate(int output, int expected) {
@@ -9,5 +10,24 @@ public class App {
         } else {
             System.out.println("\u001B[31mTest failed!\u001B[0m");
         }
+    }
+
+    private static void testRomanNumerals() {
+        RomanNumeral rn = new RomanNumeral();
+
+        int output_I = rn.convert("I");
+        evaluate(output_I, 1);
+
+        int output_V = rn.convert("V");
+        evaluate(output_V, 5);
+
+        int output_IV = rn.convert("IV");
+        evaluate(output_IV, 4);
+
+        int output_IIII = rn.convert("IIII");
+        evaluate(output_IIII, -1);
+
+        int output_IVVI = rn.convert("IVVI");
+        evaluate(output_IVVI, -1);
     }
 }
