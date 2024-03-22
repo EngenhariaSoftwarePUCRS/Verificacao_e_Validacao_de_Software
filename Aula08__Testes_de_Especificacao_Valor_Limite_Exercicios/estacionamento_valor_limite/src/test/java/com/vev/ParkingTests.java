ed741427696c4f713a5ddac506f8426be5809736
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-03-21 23:14:07 -0300

    Tests Template

---
+++ Aula08__Testes_de_Especificacao_Valor_Limite_Exercicios/estacionamento_valor_limite/src/test/java/com/vev/ParkingTests.java
@@ -0,0 +1,42 @@
+package com.vev;
+
+import org.junit.jupiter.api.Test;
+
+
+public class ParkingTests {
+    
+    @Test
+    public void upTo15MinutesIsFree() {
+
+    }
+
+    @Test
+    public void upTo120MinutesIsFixedPrice() {
+
+    }
+
+    @Test
+    public void upTo120MinutesIsFixedPriceAndHalfForVIP() {
+
+    }
+
+    @Test
+    public void over120MinutesNotOvernightHasAdditionalFee() {
+
+    }
+
+    @Test
+    public void over120MinutesNotOvernightHasAdditionalFeeAndHalfForVIP() {
+
+    }
+
+    @Test
+    public void overnightHasDifferentPrice() {
+
+    }
+
+    @Test
+    public void overnightHasDifferentPriceAndHalfForVIP() {
+
+    }
+}
